af03bc481db81dc83241cdfc6b5bbebadff5dacb
Myrellags <myrellags@gmail.com>
2021-06-07 16:00:38 -0300

    Add project files.

---
+++ WorldJourney/Controllers/TravelerController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorldJourney.Controllers
+{
+    public class TravelerController : Controller
+    {
+        public IActionResult Index(string name)
+        {
+            ViewBag.VisiterName = name;
+            return View();
+        }
+
+    }
+}
